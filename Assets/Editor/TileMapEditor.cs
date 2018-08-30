4b4009e456aefea5357ca36d4c6eca8af24f9894
Dayo <admin@nerdroco.com>
2018-08-30 13:09:59 -0700

    TileMap
    Added TileMap script and updated TileMapEditor

--- Assets/Editor/TileMapEditor.cs
+++ Assets/Editor/TileMapEditor.cs
@@ -8,6 +8,8 @@ public class TileMapEditor {
     [MenuItem("GameObject/TileMap")]
     public static void CreateTileMap()
     {
+        GameObject go = new GameObject("TileMap");
+        go.AddComponent<TileMap>();
         Debug.Log("Added Tilemap");
     }
 }
